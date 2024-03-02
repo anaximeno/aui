7a17c47dcf0dcf5254dee53dcce6862514e89b50
Anaxímeno Brito <anaximenobrito@gmail.com>
2024-03-02 17:55:43 -0100

    feat: Add Entry Dialgo Window
    refactor: Move dialog decl to subclasses

--- aui.py
+++ aui.py
@@ -12,24 +12,7 @@ from gi.repository import Gtk
 
 
 class DialogWindow(Gtk.Window):
-    def __init__(
-        self,
-        message_type,
-        content_message: str,
-        title: str = None,
-        content_title: str = None,
-        buttons=None,
-    ) -> None:
-        super().__init__(title=title)
-        self.dialog = Gtk.MessageDialog(
-            flags=0,
-            transient_for=self,
-            title=title,
-            message_type=message_type,
-            text=content_title,
-            buttons=buttons,
-        )
-        self.dialog.format_secondary_text(content_message)
+    dialog: Gtk.Dialog
 
     def run(self):
         return self.dialog.run()
@@ -41,12 +24,15 @@ class DialogWindow(Gtk.Window):
 
 class InfoDialogWindow(DialogWindow):
     def __init__(self, message: str, title: str = None) -> None:
-        super().__init__(
-            message_type=Gtk.MessageType.INFO,
+        super().__init__(title=title)
+        self.dialog = Gtk.MessageDialog(
+            flags=0,
+            transient_for=self,
             title=title,
-            content_message=message,
+            message_type=Gtk.MessageType.INFO,
             buttons=Gtk.ButtonsType.OK,
         )
+        self.dialog.format_secondary_text(message)
 
 
 class QuestionDialogWindow(DialogWindow):
@@ -58,21 +44,78 @@ class QuestionDialogWindow(DialogWindow):
         message: str,
         title: str = None,
     ) -> None:
-        super().__init__(
-            message_type=Gtk.MessageType.QUESTION,
-            content_message=message,
+        super().__init__(title=title)
+        self.dialog = Gtk.MessageDialog(
+            flags=0,
+            transient_for=self,
             title=title,
+            message_type=Gtk.MessageType.QUESTION,
             buttons=Gtk.ButtonsType.YES_NO,
         )
+        self.dialog.format_secondary_text(message)
 
     def run(self):
         """Returns `y` if the user clicked yes, or `n` if the user clicked no.
         If no response was given (by closing the window or clicking escape) it
         returns python `None`.
         """
-        result = super().run()
-        if result == Gtk.ResponseType.YES:
+        response = super().run()
+        if response == Gtk.ResponseType.YES:
             return self.RESPONSE_YES
-        elif result == Gtk.ResponseType.NO:
+        elif response == Gtk.ResponseType.NO:
             return self.RESPONSE_NO
+        return None
+
+
+class EntryDialog(Gtk.Dialog):
+    def __init__(
+        self,
+        title: str = None,
+        label: str = None,
+        default_text: str = "",
+        **kwargs,
+    ):
+        super().__init__(title=title, **kwargs)
+        self.add_buttons(
+            Gtk.STOCK_CANCEL,
+            Gtk.ResponseType.CANCEL,
+            Gtk.STOCK_OK,
+            Gtk.ResponseType.OK,
+        )
+        self._box = Gtk.HBox()
+
+        if label is not None:
+            self._label = Gtk.Label(label=label)
+            self._box.pack_start(self._label, False, False, 0)
+
+        self.entry = Gtk.Entry(text=default_text)
+        self._box.pack_start(self.entry, True, True, 0)
+
+        self._content_area = self.get_content_area()
+        self._content_area.add(self._box)
+        self.set_default_size(150, 100)
+        self.show_all()
+
+
+class EntryDialogWindow(DialogWindow):
+    def __init__(
+        self,
+        title: str = None,
+        label: str = None,
+        default_text: str = "",
+    ) -> None:
+        super().__init__(title=title)
+        self.dialog = EntryDialog(
+            flags=0,
+            transient_for=self,
+            title=title,
+            label=label,
+            default_text=default_text,
+        )
+
+    def run(self):
+        """Returns entry text if user clicked ok, else returns None."""
+        response = super().run()
+        if response == Gtk.ResponseType.OK:
+            return self.dialog.entry.get_text()
         return None
